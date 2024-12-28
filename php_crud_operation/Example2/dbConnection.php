ef1832e0d0c706c26d366230a8797bb296532514
Sunny Wali <sunnywali777@gmail.com>
2024-12-28 14:57:29 +0530

    made some changes

---
+++ php_crud_operation/Example2/dbConnection.php
@@ -0,0 +1,13 @@
+<?php
+$serverName="localhost",
+$userName="root",
+$password="",
+$dbName="University",
+
+$conn=new mysqli($serverName,$userName,$password,$dbName);
+
+if($conn->connect_error)
+{
+    die("Connection failed:".$conn->connect_error);
+}
+?>
