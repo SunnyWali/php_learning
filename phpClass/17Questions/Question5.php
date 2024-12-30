dcd588db0fb887e2b4eae48aa0efd689d544f591
Sunny Wali <sunnywali777@gmail.com>
2024-12-30 22:54:45 +0530

    made some changes

---
+++ phpClass/17Questions/Question5.php
@@ -0,0 +1,26 @@
+<?php
+$array=[1,2,96,4,4,15,8,8,9,9];
+$result=[];
+$j=0;
+for($i=0;$i<count($array);$i++)
+{
+    for($j=$i+1;$j<count($array);$j++)
+    {
+        if($array[$i]>$array[$j])
+        {
+            $temp=$array[$i];
+            $array[$i]=$array[$j];
+            $array[$j]=$temp;
+        }
+    }
+}
+for($i=0;$i<count($array)-1;$i++)
+{
+    if($array[$i]!=$array[$i+1])
+    {
+        $result[$j++]=$array[$i];
+    }
+}
+$result[$j++]=$array[count($array)-1];
+print_r($result);
+?>
