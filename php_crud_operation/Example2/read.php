72719cfe00e0b5e8bcc75d95a4eb0390cf713948
Sunny Wali <sunnywali777@gmail.com>
2025-01-03 14:44:21 +0530

    made some changes

--- php_crud_operation/Example2/read.php
+++ php_crud_operation/Example2/read.php
@@ -3,7 +3,7 @@ include "dbConnection.php";
 $sql="Select * from students";
 $result=$conn->query($sql);
 if($result->num_rows > 0){
-    echo"<table border='1'><tr><th>ID</th><th>Name</th><th>Email</th><th>Phone</th>M/tr>";
+    echo"<table border='1'><tr><th>ID</th><th>Name</th><th>Email</th><th>Phone</th></tr>";
     while($row=$result->fetch_assoc()){
         echo"<tr>
         <td>".$row["id"]."</td>
