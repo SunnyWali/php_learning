5a81add44deaf9dc391d52695e823fd1bb6d35d1
Sunny Wali <sunnywali777@gmail.com>
2025-01-09 20:04:10 +0530

    made some changes

--- phpClass/Tutorial1/Question1.php
+++ phpClass/Tutorial1/Question1.php
@@ -1,6 +1,25 @@
 <?php
-$num1=readline("enter the first number");
-$num2=readline("enter the second number");
-$sum=$num1+$num2;
-echo $sum;
+function sum($num1,$num2)
+{
+    return $num1+$num2;
+}
+function diff($num1,$num2)
+{
+    return $num1-$num2;
+}
+function mul($num1,$num2)
+{
+    return $num1*$num2;
+}
+function div($num1,$num2)
+{
+    return $num1/$num2;
+}
+echo"The sum of the given numbers are: ".sum(10,20);
+echo "\n";
+echo"The difference of the given numbers are: ".diff(10,20);
+echo "\n";
+echo"The multiplication of the given numbers are: ".mul(10,20);
+echo "\n";
+echo"The division of the given numbers are: ".div(10,20);
 ?>
--- phpClass/Tutorial1/Question2(b).php
+++
@@ -1,9 +0,0 @@
-<?php
-$string=readline("enter the string");
-$reversed=" ";
-for($i=strlen($string)-1;$i>=0;$i--)
-{
-    $reversed.=$string[$i];
-}
-echo $reversed;
-?>
